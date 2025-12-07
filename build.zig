b004f5c933b032dd63837edf5614ccd79d3c6218
小笠原 楓人 <rock-db@outlook.jp>
2025-12-07 09:47:26 +0900

    Rename executable from 'hclos' to 'cpsi'

--- build.zig
+++ build.zig
@@ -106,7 +106,7 @@ pub fn build(b: *std.Build) void {
     } });
 
     const exe = b.addExecutable(.{
-        .name = "hclos",
+        .name = "cpsi",
         .linkage = .static,
         .root_module = b.createModule(.{ .root_source_file = b.path("src/main.zig"), .target = target, .optimize = optimize, .imports = &.{
             .{ .name = "fetch", .module = fetch },
